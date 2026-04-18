6727dc0945f41fcb05fc93a96c08510dfbaf40d5
Soumyadipta137 <soumyadiptadpt@gmail.com>
2026-04-18 15:48:47 +0530

    GUI Initiated

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 DATA_FOLDER = "Data"
-INPUT_FILE = "Data/input/sample.wav"
+INPUT_FILE = "Data/input/Official.mpeg"
 CLEAN_AUDIO_FILE = "Data/input/clean_audio.wav"
 OUTPUT_FOLDER = "Data/output"
 OUTPUT_VISUALS = "Data"
+
 SAMPLE_RATE = 64000
